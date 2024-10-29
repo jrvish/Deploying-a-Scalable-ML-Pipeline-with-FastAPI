e225116965cc09acbdccc8e1b199b3d6e6050d51
jrvish <91713981+jrvish@users.noreply.github.com>
2024-10-28 21:50:17 -0600

    Updates to test_ml

--- test_ml.py
+++ test_ml.py
@@ -1,13 +1,31 @@
 import pytest
+import pandas as pd
+import numpy as np
+import os
+from ml import data, model
+from sklearn.linear_model import LogisticRegression
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_one():
+
+    #Here is my descrption
+
     """
     # add description for the first test
     """
     # Your code here
-    pass
+    # Return the expected type of result
+   
+    # Your code here
+    X_train = np.random.rand(100, 25)
+    y_train = np.random.randint(2, size=100)
+
+    #Training the model
+    model_output = model.train_model(X_train, y_train)
+
+    #Check model type
+    assert isinstance(model_output, LogisticRegression)
 
 
 # TODO: implement the second test. Change the function name and input as needed
