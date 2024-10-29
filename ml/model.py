d8de09eb5ca9dfe0c2a989d0e42535451b70bcb3
jrvish <91713981+jrvish@users.noreply.github.com>
2024-10-28 22:35:08 -0600

    Changed the model type I wanted to use in model.py

--- ml/model.py
+++ ml/model.py
@@ -2,7 +2,7 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -20,10 +20,14 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-   # TODO: implement the function *DONE
-    random_forest_model = RandomForestClassifier()
-    random_forest_model.fit(X_train, y_train)
-    return random_forest_model
+   # TODO: implement the function - I ws to use Logistic Regression*DONE
+    model = LogisticRegression()
+
+    ##Fitting the model to adjust parameters to minimize errors
+    model.fit(X_train, y_train)
+
+    #Returning the trained model
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -78,7 +82,7 @@ def save_model(model, path):
     """
     # TODO: implement the function *DONE
     with open(path,'wb') as file:
-        pickle.dump(model,file)
+        pickle.dump(model,file) #serializes the oject to be dumped into this file
     
 
 def load_model(path):
@@ -131,10 +135,10 @@ def performance_on_categorical_slice(
     X_slice, y_slice, _, _ = process_data(
         sliced_data,
         categorical_features = categorical_features,
-        label = label,
+        label = "salary",
         training = False,
         encoder = encoder,
-        lb = lb,
+        lb = lb
 
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
--- model_card_template.md
+++ model_card_template.md
@@ -4,10 +4,14 @@ For additional information see the Model Card paper: https://arxiv.org/pdf/1810.
 
 ## Model Details
 
+A Logical Regession model was used in this analysis.
+
 ## Intended Use
+The intended use of this model was to analyze the Census Income Data from a 1994 Census dataset. The goal was to predict whether someone made over $50,000 per year based on the census data. The overall project was to teach us about how to deploy a Machine Learning pipeline. 
 
 ## Training Data
-
+The data used came from a 1994 Census Income dataset.
+There are 14 features in the dataset that are either Integers or Categorical. 
 ## Evaluation Data
 
 ## Metrics
